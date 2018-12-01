155c5cdc658e90343fc9c44a78c3a7ddcc49580e
KurtisAD <KurtisAD@users.noreply.github.com>
2018-12-01 17:58:36 -0500

    Completion of PP1
    Added plotting code
    Corrected dt to a proper value
    Tested values of dt

--- PP1.m
+++ PP1.m
@@ -5,7 +5,7 @@ function PP1
     f = @(x) sin(x);
     
     % Scheme parameters
-    N = 10; L = pi; dx = L/N; dt = 0.2; tmax = floor(1/dt);
+    N = 10; L = pi; dx = L/N; dt = 0.1; tmax = 1/dt+1;
     
     % The initial value for the schemes
     U_0 = f(dx * (1:N-1));
@@ -16,9 +16,41 @@ function PP1
     E1 = calculateError(S1_U,u,dx,dt,N,tmax);
     E2 = calculateError(S2_U,u,dx,dt,N,tmax);
     
+    % Plotting
+    % We need to plot for timeslots 0.1, 0.5 and 1
+    chi = dx * (0:N);
+    
+    L1 = plot(chi,[0,U_0(1,:),0],'k');
+
+    
+    % Scheme plot
+    figure(1);
     hold on;
-    semilogy(E1, '--b');
-    semilogy(E2, '--r');
+    title('Scheme plot at t=0, 0.1, 0.5, and 1.0');
+    ylabel('Heat');
+    xlabel('x position');
+    % S1
+    L2 = plot(chi,[0,S1_U(0.1/dt+1,:),0],'b');
+    plot(chi,[0,S1_U(0.5/dt+1,:),0],'b');
+    plot(chi,[0,S1_U(1/dt+1,:),0],'b');
+    
+    % S2
+    L3 = plot(chi,[0,S2_U(0.1/dt+1,:),0],'r');
+    plot(chi,[0,S2_U(0.5/dt+1,:),0],'r');
+    plot(chi,[0,S2_U(1/dt+1,:),0],'r');
+    hold off;
+    
+    legend([L1,L2,L3],'f(x)','S1','S2');
+    % Error plot
+    figure(2);
+    hold on;
+    title('Error plot');
+    ylabel('Average error');
+    xlabel('Time');
+    semilogy(dt * (0:tmax-1),E1, '--b');
+    semilogy(dt * (0:tmax-1),E2, '--r');
+    legend('S1','S2');
+    hold off;
     
     %
     % Function Definitions
